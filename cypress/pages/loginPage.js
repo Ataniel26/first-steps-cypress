54664ee557459d86ce9fdca4ea10a9e433f84f20
atanielalves <atanielalvesoliveira26@gmail.com>
2025-09-14 11:15:21 +1000

    Add page object

---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,23 @@
+class LoginPage {
+   selectorsList() {
+    const selectors = {
+      usernameFild: '[name="username"]',
+      passwordFild: '[name="password"]',
+      submitFild: "[type='submit']",
+      wrongCredentialAlert: ".oxd-alert",
+    }
+    return selectors
+  }
+
+  accessLoginPage() {
+    cy.visit('/auth/login')
+  }
+
+  loginWithAnyUser(username, password) {
+    cy.get(this.selectorsList().usernameFild).type(username)
+    cy.get(this.selectorsList().passwordFild).type(password)
+    cy.get(this.selectorsList().submitFild).click()
+  }
+}
+
+export default LoginPage
